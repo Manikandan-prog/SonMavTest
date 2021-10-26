8bf31b96561045efd0cb8e921b143e5e6dad4505
Manikandan-prog <84707867+Manikandan-prog@users.noreply.github.com>
2021-10-26 14:24:09 +0530

    Add files via upload

---
+++ demo02/src/test/java/demo02/FooServiceShould.java
@@ -0,0 +1,37 @@
+package demo02;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import com.demo.FooService;
+
+import nl.altindag.log.LogCaptor;
+
+
+public class FooServiceShould {
+
+
+    @Test
+
+    public void logInfoAndWarnMessages() {
+
+        String expectedInfoMessage = "Keyboard not responding. Press any key to continue...";
+
+        String expectedWarnMessage = "Congratulations, you are pregnant!";
+
+
+        LogCaptor logCaptor = LogCaptor.forClass(FooService.class);
+
+
+        FooService fooService = new FooService();
+
+        fooService.sayHello();
+
+
+        assertThat(logCaptor.getInfoLogs().contains(expectedInfoMessage));
+
+        assertThat(logCaptor.getWarnLogs().contains(expectedWarnMessage));
+
+    }
+
+}
